050ba8cacde821bf1239fd90896afb4ffc5f8d74
Kedon <valdenir@kedon.com.br>
2022-11-19 18:15:20 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Layout from './layout/Layout';
+import Home from './screens/home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Layout component={Home} />
+  );
+}
+
+export default App;
